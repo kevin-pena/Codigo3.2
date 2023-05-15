e138cc57137bfe7f6ccb2d139aaf64794f0e166a
kevin <kevin@C>
2023-05-14 23:35:22 -0500

    Agregar archivos de proyecto.

---
+++ Codigo3.2/MainForm.vb
@@ -0,0 +1,19 @@
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Creación de objetos utilizando diferentes constructores
+        objeto1 = New MiClase()
+        objeto2 = New MiClase(10, "Hola")
+
+        ' Llamada al método MostrarVariables
+        MessageBox.Show("Objeto 1:")
+        objeto1.MostrarVariables()
+
+        MessageBox.Show(vbCrLf & "Objeto 2:")
+        objeto2.MostrarVariables()
+
+        ' Liberación de memoria utilizando el recolector de basura
+        objeto1 = Nothing
+        objeto2 = Nothing
+        GC.Collect()
+    End Sub
+End Class
---
+++ Codigo3.2/MiClase.vb
@@ -0,0 +1,34 @@
+﻿Public Class MiClase
+    ' Variables de instancia
+    Private variableEntera As Integer
+    Private variableCadena As String
+
+    ' Constructor por defecto
+    Public Sub New()
+        variableEntera = 0
+        variableCadena = "Sin valor"
+    End Sub
+
+    ' Constructor con parámetros
+    Public Sub New(entero As Integer, cadena As String)
+        variableEntera = entero
+        variableCadena = cadena
+    End Sub
+
+    ' Destructor
+    Protected Overrides Sub Finalize()
+        MessageBox.Show("Objeto destruido")
+        MyBase.Finalize()
+    End Sub
+
+    ' Método que muestra el valor de las variables
+    Public Sub MostrarVariables()
+        MessageBox.Show("Variable Entera: " & variableEntera.ToString() & vbCrLf & "Variable Cadena: " & variableCadena)
+    End Sub
+End Class
+
+Public Class MainForm
+    Private objeto1 As MiClase
+    Private objeto2 As MiClase
+
+End Class
